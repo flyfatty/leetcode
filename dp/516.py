dd1d42fb4810d1f9d48df145dc53188727d1129d
liubin <28278672@qq.com>
2020-11-28 00:13:04 +0800

    最长回文子序列 新增三角dp解法

--- dp/516.py
+++ dp/516.py
@@ -1,9 +1,10 @@
 """最长回文子序列
 关键字:  DP /  DFS
-思路1: 倒序+最长公共子序列
-思路2: 最长公共子序列的DFS思路
-思路3: 优化成三角DP
+思路1: DP 倒序+最长公共子序列
+思路2: DFS 最长公共子序列的DFS思路
+思路3: DP 优化成三角DP
 """
+from pprint import pprint
 class Solution:
     def longestPalindromeSubseq1(self, s: str) -> int:
         def lcs( s1 , s2 ):
@@ -34,3 +35,19 @@ class Solution:
             visit[(i,j)] = ret
             return ret
         return dfs( 0, len(s)-1 )
+
+
+    def longestPalindromeSubseq(self, s: str) -> int:
+        ls = len(s)
+        dp = [[0]*ls for _ in range(ls)]
+        for i in range(ls-1, -1, -1):
+            for j in range(i, ls):
+                if i == j:
+                    dp[i][j] = 1
+                elif s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        return dp[0][ls-1]
+
+pprint(Solution().longestPalindromeSubseq("bbbab"))
